f731b2f92cbc97f2400d7167185dfc0426938e03
Ysa <nyeomi4@gmail.com>
2026-09-23 14:59:40 -0300

    Delete

--- Insert.sql
+++
@@ -1,8 +0,0 @@
-
-INSERT INTO usuarios (nome, email, senha) VALUES 
-('Maria Silva', 'maria@email.com', '123456'),
-('João Souza', 'joao@email.com', 
-  
-INSERT INTO artigos (titulo, conteudo) VALUES 
-('Ansiedade: como controlar', 'Dicas para lidar com a ansiedade no dia a dia...'),
-('Importância do sono', 'Dormir bem melhora a saúde mental e física...');
