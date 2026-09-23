f5d0620e166238fb5478fe8e7d44d017038feccd
Ysa <nyeomi4@gmail.com>
2026-09-23 14:28:37 -0300

    Add SQL commands for updating and deleting records
    UPDATE e DELETE

---
+++ Update_Delete.sql
@@ -0,0 +1,9 @@
+-- Atualizar 
+UPDATE usuarios SET nome = 'Maria Santos' WHERE id = 1;
+
+UPDATE usuarios SET senha = 'novaSenha123' WHERE id = 2;
+
+-- Deletar
+DELETE FROM usuarios WHERE id = 2;
+
+DELETE FROM artigos WHERE id = 1;
